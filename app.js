f222a2372bbe1add067cc7a7a6c68d9ae4ffe0d3
RodrigoA15 <rapapamija@misena.edu.co>
2023-09-20 17:22:15 -0500

    cors configuration

--- app.js
+++ app.js
@@ -15,7 +15,12 @@ import cors from "cors";
 
 const ruta = "/api";
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 app.use(morgan("dev"));
 app.use(express.json());
