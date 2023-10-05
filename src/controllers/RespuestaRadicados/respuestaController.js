37eb2ef82d8a7135769bf9c628e2e4e62e5394d0
RodrigoA15 <rapapamija@misena.edu.co>
2023-10-05 17:21:01 -0500

    query radicados respuestas

--- src/controllers/RespuestaRadicados/respuestaController.js
+++ src/controllers/RespuestaRadicados/respuestaController.js
@@ -1,4 +1,5 @@
 import Respuesta from "../../models/respuesta_radicado.js";
+import multer from "multer";
 
 export const getAllRespuestas = async (req, res) => {
   try {
@@ -37,5 +38,71 @@ export const createRespuesta = async (req, res) => {
     }
   } catch (error) {
     res.status(500).json(`Error createRespuesta ${error}`);
+    console.log(error);
+  }
+};
+
+export const savedFilePDF = async (req, res) => {
+  try {
+    const storage = multer.diskStorage({
+      destination: (req, file, cb) => {
+        cb(null, "C:\\Users\\SISTEMAS\\Documents");
+      },
+
+      filename: (req, file, cb) => {
+        // Guarda el archivo con el nombre original
+        cb(null, file.originalname);
+      },
+    });
+
+    const upload = multer({
+      storage,
+      dest: "./Documents",
+      fileFilter: (req, file, cb) => {
+        const filetypes = /pdf/;
+        const mimetype = filetypes.test(file.mimetype);
+
+        if (mimetype) {
+          return cb(null, true);
+        }
+
+        cb("Tipo de archivo no valido");
+      },
+    });
+
+    upload.single("respuesta_pdf")(req, res, async (err) => {
+      if (err) {
+        return res.status(500).json({ message: "Error creating filed" + err });
+      }
+
+      res.status(200).json({ message: "Archivo cargado exitosamente" });
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const respuestasporRadicado = async (req, res) => {
+  try {
+    const response = await Respuesta.find({})
+      .populate({
+        path: "id_asignacion",
+        populate: {
+          path: "id_radicado",
+          match: { numero_radicado: req.params.numero_radicado },
+        },
+      })
+      .exec();
+    const validacion = response.filter((respuesta) => {
+      return respuesta.id_asignacion.id_radicado !== null;
+    });
+
+    if (validacion.length > 0) {
+      res.status(200).json(validacion);
+    } else {
+      res.status(404).json("No se encontraron asignaciones pendientes");
+    }
+  } catch (error) {
+    res.status(500).json(`error respuestas por radicado ${error}`);
   }
 };
--- src/routes/RespuestaRadicados/respuesta.router.js
+++ src/routes/RespuestaRadicados/respuesta.router.js
@@ -2,11 +2,17 @@ import { Router } from "express";
 import {
   getAllRespuestas,
   createRespuesta,
+  savedFilePDF,
+  respuestasporRadicado,
 } from "../../controllers/RespuestaRadicados/respuestaController.js";
 
 const router = Router();
 
 router.get("/respuestas", getAllRespuestas);
 router.post("/create_respuestas", createRespuesta);
+//ruta para guardar archivos pdf
+router.post("/filepdf", savedFilePDF);
+//
+router.get("/radicados_respuestas/:numero_radicado", respuestasporRadicado);
 
 export default router;
