ba20ccb16eb82724e6f10da8658cd979c3376443
RodrigoA15 <rapapamija@misena.edu.co>
2023-12-27 16:11:06 -0500

    Cambio de contraseña

---
+++ src/controllers/Auth/updateInfo.js
@@ -0,0 +1,34 @@
+import bcrypt from "bcrypt";
+import User from "../../models/users.js";
+
+export const updatePassword = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const { password } = req.body;
+
+    // Validación de contraseña
+    if (!password) {
+      return res
+        .status(400)
+        .json({ error: "La contraseña no puede estar vacía" });
+    }
+
+    const passwordHash = await bcrypt.hash(password, 10);
+    const data = await User.findByIdAndUpdate(
+      id,
+      { password: passwordHash },
+      { new: true }
+    );
+
+    console.log(data);
+
+    if (data) {
+      res.status(200).json({ message: "Contraseña Actualizada" });
+    } else {
+      res.status(404).json({ error: "Usuario no encontrado" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Error al actualizar la contraseña" });
+  }
+};
--- src/routes/Auth/auth.router.js
+++ src/routes/Auth/auth.router.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import * as Auth from "../../controllers/Auth/login.auth.js";
 import * as Register from "../../controllers/Auth/register.auth.js";
+import * as PasswordUPD from "../../controllers/Auth/updateInfo.js";
 
 const router = Router();
 
@@ -9,5 +10,6 @@ router.post("/register", Register.register);
 router.post("/login", Auth.login);
 router.post("/logout", Auth.logout);
 router.get("/verify", Auth.verifyToken);
+router.put("/updatepass/:id", PasswordUPD.updatePassword);
 
 export default router;
